fe4fca4f8e293a1101ee3bdf0523100c522fc003
BalzamSergey <112899383+dezzerss@users.noreply.github.com>
2023-03-28 11:18:52 +0300

    upd

--- matrix/src/Matrix.java
+++ matrix/src/Matrix.java
@@ -30,3 +30,87 @@ public class Matrix {
         return "Матрица 'matrix' имеет след. вид: " + Arrays.deepToString(matrix);
     }
 }
+
+
+
+
+/////////
+import java.util.Arrays;
+
+import java.util.Random;
+
+public class Matrix {
+    private int[][] matrix;
+
+    Matrix(int a) {
+        matrix = new int[a][a];
+        Random rand = new Random();
+
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = rand.nextInt(10);
+            }
+        }
+    }
+
+    public int summ() {
+        int summ = 0;
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (i < j) {
+                    summ += matrix[i][j];
+                }
+
+            }
+        }
+        return summ;
+    }
+
+    public int multiply() {
+        int multiplication = 1;
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    multiplication *= matrix[i][j];
+                }
+            }
+
+        }
+        return multiplication;
+    }
+
+    public int substrct() {
+        int subtraction = 0;
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (i > j) {
+                    subtraction -= matrix[i][j];
+                }
+            }
+        }
+        return subtraction;
+    }
+
+
+    @Override
+    public String toString() {
+        return Arrays.deepToString(matrix);
+    }
+
+}
+////////////
+MAIN 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размерность матрицы: ");
+        int a = sc.nextInt();
+        Matrix matrix = new Matrix(a);
+        System.out.println(matrix);
+        System.out.println("Сумма: " + matrix.summ());
+        System.out.println("Произведение: " + matrix.multiply());
+        System.out.println("Вычитание: " + matrix.substrct());
+    }
+}
