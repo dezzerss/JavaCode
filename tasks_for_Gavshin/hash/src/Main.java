f21fab6a3e875f2171b1068b5c45d7457602663d
Sergey Balzam <112899383+dezzerss@users.noreply.github.com>
2023-05-30 16:19:29 +0300

    main

--- tasks_for_Gavshin/hash/src/Main.java
+++ tasks_for_Gavshin/hash/src/Main.java
@@ -1,52 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        HashTable hashTable = new HashTable(10);
+        HashTable hashTable = new HashTable(16);
 
         hashTable.put("key1", "1");
         hashTable.put("key2", "432");
         hashTable.put("key3", "324");
         hashTable.put("key4", "23");
+        hashTable.put("key5", "2233");
 
         hashTable.remove("key3");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите команду, для дальнейших действий (Таблица, Добавить, Удалить, Выйти): ");
-        String command = sc.nextLine();
-        if (command.equalsIgnoreCase("Таблица")) {
-            displayHashTable(hashTable);
-        } else if (command.equalsIgnoreCase("Добавить")) {
-            addElement(hashTable);
-        } else if (command.equalsIgnoreCase("Удалить")) {
-            removeElement(hashTable);
-        } else if (command.equalsIgnoreCase("Выйти")) {
-            System.out.println("Всего хорошего");
-        } else {
-            System.out.println("Неверная команда");
-        }
-    }
-    private static void removeElement(HashTable hashTable) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите ключ элемента для удаления: ");
-        String key = scanner.nextLine();
-        boolean removed = hashTable.remove(key);
-        if (removed) {
-            System.out.println("Элемент удален");
-        } else {
-            System.out.println("Элемент не найден");
-        }
-    }
-    private static void addElement(HashTable hashTable) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите ключ: ");
-        String key = scanner.nextLine();
-        System.out.print("Введите значение: ");
-        String value = scanner.nextLine();
-        hashTable.put(key, value);
-        System.out.println("Элемент добавлен");
-    }
-    private static void displayHashTable(HashTable hashTable) {
+        System.out.println(hashTable.get("key2"));
+
+        System.out.println("Полученная Хеш-Таблица: ");
         for (String key : hashTable.keys()) {
             System.out.println("Key: " + key + ", Value: " + hashTable.get(key));
         }
